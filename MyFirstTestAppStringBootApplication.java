ba119d6c17d8a8acd8dad52e065964506d7b8546
Иван <150525201+IvanDvriz-130916y-10@users.noreply.github.com>
2024-09-11 08:50:39 +0500

    Add files via upload

---
+++ MyFirstTestAppStringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.dvornikov.MyFirstTestAppStringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstTestAppStringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstTestAppStringBootApplication.class, args);
+	}
+
+}
